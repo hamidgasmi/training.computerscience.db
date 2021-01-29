210a2a14a79b5ffebb37ea2378413c7fa4ff62a6
Hamid <mamid1706@hotmail.fr>
2021-01-29 04:55:02 -0800

    Add postgreSQL queries

---
+++ sql_postgreSQL_queries.sql
@@ -0,0 +1,17 @@
+--6. Index
+---- 6.1. Select existing indexes:
+SELECT schemaname, tablename, indexname, indexdef 
+  FROM pg_indexes 
+WHERE schemaname = 'schema_1'
+  AND tablename = 'table_name';
+  
+---- 6.2. Create new indexe:
+
+--7. Sessions:
+-- 7.1. Select Session
+SELECT * FROM pg_stat_activity;
+SELECT pid FROM pg_stat_activity WHERE usename='app_user' and application_name='' and datname LIKE '%hamid';
+-- 7.2. Terminate sessions
+SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE pid = 9102;
+SELECT pg_terminate_backend(pid) FROM pg_stat_activity 
+ WHERE pid IN (SELECT pid FROM pg_stat_activity) WHERE datname LIKE '%hamid');
